d877c2653711f55a82ce0242c8321d448c3c2db1
guchey <taguchi8shingen@gmail.com>
2023-10-02 09:43:36 +0900

    Impl site explorer url rating history (#33)
    * Impl site-explorer/url-rating-history
    
    * Add url rating history CI

--- src/main/kotlin/io/github/guchey/embulk/input/ahrefs/AhrefsInputPluginDelegate.kt
+++ src/main/kotlin/io/github/guchey/embulk/input/ahrefs/AhrefsInputPluginDelegate.kt
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnore
 import io.github.guchey.embulk.input.ahrefs.delegate.AhrefsBaseDelegate
 import io.github.guchey.embulk.input.ahrefs.delegate.keywordsexplorer.OverviewInputPlugin
-import io.github.guchey.embulk.input.ahrefs.delegate.siteexplorer.BackLinkStatsInputPlugin
-import io.github.guchey.embulk.input.ahrefs.delegate.siteexplorer.DomainRatingInputPlugin
-import io.github.guchey.embulk.input.ahrefs.delegate.siteexplorer.MetricsInputPlugin
-import io.github.guchey.embulk.input.ahrefs.delegate.siteexplorer.RefDomainsHistoryInputPlugin
+import io.github.guchey.embulk.input.ahrefs.delegate.siteexplorer.*
 import org.embulk.base.restclient.DispatchingRestClientInputPluginDelegate
 import org.embulk.base.restclient.RestClientInputPluginDelegate
 import org.embulk.util.config.Config
@@ -18,7 +15,7 @@ import java.util.*
 class AhrefsInputPluginDelegate : DispatchingRestClientInputPluginDelegate<AhrefsInputPluginDelegate.PluginTask>() {
     interface PluginTask : AhrefsBaseDelegate.PluginTask, OverviewInputPlugin.PluginTask,
         BackLinkStatsInputPlugin.PluginTask, DomainRatingInputPlugin.PluginTask, MetricsInputPlugin.PluginTask,
-        RefDomainsHistoryInputPlugin.PluginTask {
+        RefDomainsHistoryInputPlugin.PluginTask, UrlRatingHistoryInputPlugin.PluginTask {
 
         @get:Config("resource")
         val resource: Resource
@@ -28,6 +25,7 @@ class AhrefsInputPluginDelegate : DispatchingRestClientInputPluginDelegate<Ahref
             SITE_EXPLORER_BACKLINK_STATS(BackLinkStatsInputPlugin()),
             SITE_EXPLORER_METRICS(MetricsInputPlugin()),
             SITE_EXPLORER_REF_DOMAINS_HISTORY(RefDomainsHistoryInputPlugin()),
+            SITE_EXPLORER_URL_RATING_HISTORY(UrlRatingHistoryInputPlugin()),
             KEYWORD_EXPLORER_OVERVIEW(OverviewInputPlugin());
 
             companion object {
